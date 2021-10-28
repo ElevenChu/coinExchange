c05c5e7bba16bf31e9368f7dd0ea7780eee53f11
elevenChu <964737042@qq.com>
2021-10-28 16:51:04 +0800

    10.28-网关基础设施的搭建

---
+++ coin-iaas/gateway-server/src/main/java/com/elevenchu/GatewayServerApplication.java
@@ -0,0 +1,12 @@
+package com.elevenchu;
+import  org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class,args);
+    }
+}
