5ab14a39fa24222533690a5797348d708a2ed67c
elevenChu <964737042@qq.com>
2021-10-28 21:22:37 +0800

    10.28-网关限流规则route/自定义Api

---
+++ coin-iaas/gateway-server/src/main/java/com/elevenchu/controller/FlowRulesController.java
@@ -0,0 +1,27 @@
+package com.elevenchu.controller;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+public class FlowRulesController {
+
+
+
+    @GetMapping("/gateway")
+    public Set<GatewayFlowRule> getGatewayFlowRules(){
+        return GatewayRuleManager.getRules();
+    }
+
+    @GetMapping("/api")
+    public Set<ApiDefinition> getApiGroupRules(){
+        return GatewayApiDefinitionManager.getApiDefinitions();
+    }
+
+}
