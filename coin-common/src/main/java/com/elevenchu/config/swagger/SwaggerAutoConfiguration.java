398ff035dba6a3e88c964766220a6149b5fbc279
elevenChu <964737042@qq.com>
2021-11-07 21:00:06 +0800

    11.6-Common中swagger

--- coin-common/src/main/java/com/elevenchu/config/swagger/SwaggerAutoConfiguration.java
+++ coin-common/src/main/java/com/elevenchu/config/swagger/SwaggerAutoConfiguration.java
@@ -1,26 +1,82 @@
 package com.elevenchu.config.swagger;
 
+import org.checkerframework.checker.units.qual.A;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
-
+@EnableSwagger2
+@EnableConfigurationProperties(SwaggerProperties.class)
 public class SwaggerAutoConfiguration {
- public Docket docket(){
-     Docket docket = new Docket(DocumentationType.SWAGGER_2)
-             .apiInfo(apiInfo())
-             .select()
-             .apis(RequestHandlerSelectors.basePackage("com.elevenchu.controller"))
-             .paths(PathSelectors.any())
-             .build();
-    return docket;
- }
 
+    private SwaggerProperties swaggerProperties;
+
+    public SwaggerAutoConfiguration(SwaggerProperties swaggerProperties) {
+        this.swaggerProperties = swaggerProperties;
+    }
+
+    @Bean
+    public Docket docket() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
+                .paths(PathSelectors.any())
+                .build();
+        // 安全的配置
+        docket.securitySchemes(securitySchemes()) // 安全规则
+                .securityContexts(securityContexts()); // 安全配置的上下问
+        return docket;
+    }
+
+    /**
+     * Api信息的简介
+     * @return
+     */
     private ApiInfo apiInfo() {
- return    null;
- }
+        return new ApiInfoBuilder().contact(
+                new Contact(swaggerProperties.getName(), swaggerProperties.getUrl(), swaggerProperties.getEmail())
+        )
+                .title(swaggerProperties.getTitle())
+                .description(swaggerProperties.getDescription())
+                .version(swaggerProperties.getVersion())
+                .termsOfServiceUrl(swaggerProperties.getTermsOfServiceUrl())
+                .build();
+
+    }
+
+    /**
+     * 安全的上下文
+     * @return
+     */
+    private List<SecurityContext> securityContexts() {
+        return Arrays.asList(new SecurityContext(
+                Arrays.asList(new SecurityReference("Authorization", new AuthorizationScope[]{new AuthorizationScope("global", "accessResource")})),
+                PathSelectors.any()
+        ));
+    }
+
+    /**
+     * 安全规则的配置
+     * @return
+     */
+    private List<SecurityScheme> securitySchemes() {
+        return  Arrays.asList(new ApiKey("Authorization", "Authorization", "Authorization"));
+    }
+
+
 }
