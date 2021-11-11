a3793bc260cd3b4dc0579bdff4b065cb32594089
elevenChu <964737042@qq.com>
2021-11-11 22:58:35 +0800

    11.11-配置文件

---
+++ coin-admin/admin-service/src/main/java/com/elevenchu/AdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elevenchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AdminServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServiceApplication.class,args);
+    }
+}
