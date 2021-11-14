907aa23c85154be19db08dcbeeda81fea498533d
elevenChu <964737042@qq.com>
2021-11-14 22:27:11 +0800

    11.14-feign与JWT返回类

--- coin-admin/admin-service/src/main/java/com/elevenchu/AdminServiceApplication.java
+++ coin-admin/admin-service/src/main/java/com/elevenchu/AdminServiceApplication.java
@@ -3,10 +3,12 @@ package com.elevenchu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class AdminServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class,args);
--- coin-admin/admin-service/src/main/java/com/elevenchu/service/impl/SysLoginServiceImpl.java
+++ coin-admin/admin-service/src/main/java/com/elevenchu/service/impl/SysLoginServiceImpl.java
@@ -1,9 +1,11 @@
 package com.elevenchu.service.impl;
 
 import com.elevenchu.domain.SysMenu;
+import com.elevenchu.feign.OAuth2FeignClient;
 import com.elevenchu.model.LoginResult;
 import com.elevenchu.service.SysLoginService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import java.util.List;
 @Service
 @Slf4j
 public class SysLoginServiceImpl implements SysLoginService {
+    @Autowired
+    private OAuth2FeignClient oAuth2FeignClient;
     @Override
     public LoginResult login(String username, String password) {
       log.info("用户{}开始登录",username);
