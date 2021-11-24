48e6fc0fd3e513f65b0e05ce6db4552de460b5db
elevenChu <964737042@qq.com>
2021-11-24 21:54:27 +0800

    11.24-实名认证

--- coin-member/member-service/src/main/java/com/elevenchu/controller/LoginController.java
+++ coin-member/member-service/src/main/java/com/elevenchu/controller/LoginController.java
@@ -1,15 +1,19 @@
 package com.elevenchu.controller;
 
+import com.elevenchu.domain.User;
 import com.elevenchu.model.LoginForm;
 import com.elevenchu.model.LoginUser;
 import com.elevenchu.model.R;
 import com.elevenchu.service.LoginService;
+import com.elevenchu.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Api(tags = "登录控制器")
 public class LoginController {
+    @Autowired
+    private UserService userService;
     @Autowired
     private LoginService loginService;
     @RequestMapping("/login")
@@ -30,5 +36,17 @@ public class LoginController {
         return R.ok(loginUser);
     }
 
+
+    @GetMapping("/current/info")
+    @ApiOperation("获取当前登录用户对像的信息")
+    public  R<User> currentUserInfo(){
+        String userID = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        User user = userService.getById(userID);
+        user.setPassword("***");
+        user.setPaypassword("****");
+        user.setAccessKeyId("***");
+        user.setAccessKeySecret("*****");
+        return R.ok(user);
+    }
 
 }
