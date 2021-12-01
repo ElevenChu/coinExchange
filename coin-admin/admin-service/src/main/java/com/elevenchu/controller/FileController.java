0352a6126f7ed2e12387075ae1c4467341908838
elevenChu <964737042@qq.com>
2021-12-01 21:06:16 +0800

    12.1-OSS_WEB直传身份证信息

--- coin-admin/admin-service/src/main/java/com/elevenchu/controller/FileController.java
+++ coin-admin/admin-service/src/main/java/com/elevenchu/controller/FileController.java
@@ -1,7 +1,11 @@
 package com.elevenchu.controller;
 
 import cn.hutool.core.date.DateUtil;
+import com.alibaba.fastjson.JSONObject;
 import com.aliyun.oss.OSS;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
 import com.elevenchu.model.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -9,22 +13,31 @@ import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @Api(tags = "文件上传的控制器")
 public class FileController {
     @Autowired
     private OSS ossClient;
+    @Value("${spring.cloud.alicloud.access-key}")
+    private String accessId;
     @Value("${oss.bucket.name:coin-exchange-imgs11}")
     private String bucketName;
     @Value("${spring.cloud.alicloud.oss.endpoint}")
     private String endPoint;
+
+    @Value("${oss.callback.url:129.0.0.1:9000}")
+    private String ossCallbackUrl; //
 
     @PostMapping("/image/AliYunImgUpload")
     @ApiOperation("上传文件")
@@ -42,5 +55,54 @@ public class FileController {
     }
 
 
+    @GetMapping("/image/pre/upload")
+    @ApiOperation(value = "文件的上传获取票据")
+    public R<Map<String,String>> preUploadPolicy() {
+        String dir = DateUtil.today().replaceAll("-", "/") + "/";
+        Map<String, String> policy = getPolicy(30L, 3 * 1024 * 1024L, dir);
+        return R.ok(policy) ;
+    }
+
+    private Map<String, String> getPolicy(long expireTime, long maxFileSize, String dir) {
+        try {
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
+            PolicyConditions policyConds = new PolicyConditions();
+            // 设置上传文件的最大的体积
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, maxFileSize);
+            // 设置上传文件上传到那个文件夹
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds); // 设置该Policy(票据) 有效时间
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            // 返回值
+            Map<String, String> respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId); // 设置我们之前的accessKey
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", "https://" + bucketName +"."+ endPoint); // 设置 上传的Host https://bulkname.endpoint
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            // respMap.put("expire", formatISO8601Date(expiration));
+
+            JSONObject jasonCallback = new JSONObject();
+
+            jasonCallback.put("callbackUrl", ossCallbackUrl); // 当我们的前端把文件上传到oss 服务器成功后
+            // ,oss 服务器会想回调的callbackUrl(公网) 发一个post请求来告诉后端服务器用户上传文件的情况
+            jasonCallback.put("callbackBody",
+                    "filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}");
+            jasonCallback.put("callbackBodyType", "application/x-www-form-urlencoded");
+            String base64CallbackBody = BinaryUtil.toBase64String(jasonCallback.toString().getBytes());
+            respMap.put("callback", base64CallbackBody);
+            return respMap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
