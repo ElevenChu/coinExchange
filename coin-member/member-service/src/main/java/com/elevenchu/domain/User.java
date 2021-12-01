9e6c2f8fbf32a4468c0c2e8ba2296f79179a4100
elevenChu <964737042@qq.com>
2021-12-01 15:48:20 +0800

    12.1-高级认证user实体类完善

--- coin-member/member-service/src/main/java/com/elevenchu/domain/User.java
+++ coin-member/member-service/src/main/java/com/elevenchu/domain/User.java
@@ -231,4 +231,12 @@ public class User {
     @ApiModelProperty(value="创建时间")
     private Date created;
 
+
+    @TableField(exist = false)
+    @ApiModelProperty(value = "会员的高级认证状态 0 : 审核中  1 :通过 2 : 拒绝--(拒绝的理由) 3 :未填写")
+    private Byte seniorAuthStatus ;
+
+    @TableField(exist = false)
+    @ApiModelProperty(value = "拒绝--(拒绝的理由)")
+    private String seniorAuthDesc ;
 }
