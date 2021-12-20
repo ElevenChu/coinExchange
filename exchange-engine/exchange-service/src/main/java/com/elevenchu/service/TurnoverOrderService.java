b8848bbaf2d07bca0dcf83ca3f08fc455f43945d
elevenChu <964737042@qq.com>
2021-12-20 17:07:50 +0800

    12.20-成交记录的查询(未完成

--- exchange-engine/exchange-service/src/main/java/com/elevenchu/service/TurnoverOrderService.java
+++ exchange-engine/exchange-service/src/main/java/com/elevenchu/service/TurnoverOrderService.java
@@ -1,8 +1,10 @@
 package com.elevenchu.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.elevenchu.domain.TurnoverOrder;
 import com.baomidou.mybatisplus.extension.service.IService;
 public interface TurnoverOrderService extends IService<TurnoverOrder>{
 
 
+    Page<TurnoverOrder> findByPage(Page<TurnoverOrder> page, Long userId, String symbol, Integer type);
 }
--- exchange-engine/exchange-service/src/main/java/com/elevenchu/service/impl/TurnoverOrderServiceImpl.java
+++ exchange-engine/exchange-service/src/main/java/com/elevenchu/service/impl/TurnoverOrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.elevenchu.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
@@ -10,4 +11,8 @@ import com.elevenchu.service.TurnoverOrderService;
 @Service
 public class TurnoverOrderServiceImpl extends ServiceImpl<TurnoverOrderMapper, TurnoverOrder> implements TurnoverOrderService{
 
+    @Override
+    public Page<TurnoverOrder> findByPage(Page<TurnoverOrder> page, Long userId, String symbol, Integer type) {
+        return null;
+    }
 }
