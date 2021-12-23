0f72048ea50ee4209b14cdb32daacf216c731bb1
elevenChu <964737042@qq.com>
2021-12-23 14:59:56 +0800

    12.23-撮合引擎开发前的基本环境搭建

---
+++ match-engine/match-service/src/main/java/com/elevenchu/MatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elevenchu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MatchServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MatchServiceApplication.class ,args) ;
+    }
+}
