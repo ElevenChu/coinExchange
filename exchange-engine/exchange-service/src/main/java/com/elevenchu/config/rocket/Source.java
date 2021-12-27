d680c8ebd6c486b1d6000e3c289c273403e7f45c
elevenChu <964737042@qq.com>
2021-12-27 21:49:55 +0800

    12.27-撮合系统可以初步启动,待深层检测

---
+++ exchange-engine/exchange-service/src/main/java/com/elevenchu/config/rocket/Source.java
@@ -0,0 +1,9 @@
+package com.elevenchu.config.rocket;
+
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+public interface Source {
+    @Output("order_out")
+    MessageChannel outputMessage();
+}
