2dfee1fc3fa9afb81291c5ffc9ff52ea38adab01
elevenChu <964737042@qq.com>
2021-12-29 22:10:38 +0800

    12.29-订单的匹配

--- match-engine/match-service/src/main/java/com/elevenchu/match/impl/LimitPriceMatchServiceImpl.java
+++ match-engine/match-service/src/main/java/com/elevenchu/match/impl/LimitPriceMatchServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -109,8 +110,63 @@ public class LimitPriceMatchServiceImpl implements MatchService, InitializingBea
 
     }
 
-    private ExchangeTrade processMath(Order order, Order marker, OrderBooks orderBooks) {
-        return null;
+    private ExchangeTrade processMath(Order taker, Order marker, OrderBooks orderBooks) {
+        // 1 定义交易的变量
+        // 成交的价格
+        BigDecimal dealPrice = marker.getPrice();
+        // 成交的数量
+        BigDecimal turnoverAmount = BigDecimal.ZERO;
+        // 本次需要的数量
+        BigDecimal needAmount = calcTradeAmount(taker); // 10  20
+        // 本次提供给你的数量
+        BigDecimal providerAmount = calcTradeAmount(marker); // 20 10
+
+
+        turnoverAmount = needAmount.compareTo(providerAmount) <= 0 ? needAmount : providerAmount;
+
+        if (turnoverAmount.compareTo(BigDecimal.ZERO) == 0) {
+            return null; // 无法成交
+        }
+
+        // 设置本次吃单的成交数据
+        taker.setTradedAmount(taker.getTradedAmount().add(turnoverAmount));
+        BigDecimal turnoverTaker = turnoverAmount.multiply(dealPrice).setScale(orderBooks.getCoinScale(), RoundingMode.HALF_UP);
+        taker.setTurnover(turnoverTaker);
+
+        // 设置本次挂单的成交数据
+        marker.setTradedAmount(marker.getTradedAmount().add(turnoverAmount));
+        BigDecimal markerTurnover = turnoverAmount.multiply(dealPrice).setScale(orderBooks.getBaseCoinScale(), RoundingMode.HALF_UP);
+        marker.setTurnover(markerTurnover);
+
+        ExchangeTrade exchangeTrade = new ExchangeTrade();
+
+        exchangeTrade.setAmount(turnoverAmount); // 设置购买的数量
+        exchangeTrade.setPrice(dealPrice);  // 设置购买的价格
+        exchangeTrade.setTime(System.currentTimeMillis()); // 设置成交的时间
+        exchangeTrade.setSymbol(orderBooks.getSymbol());  // 设置成交的交易对
+        exchangeTrade.setDirection(taker.getOrderDirection());  // 设置交易的方法
+        exchangeTrade.setSellOrderId(marker.getOrderId()); // 设置出售方的id
+        exchangeTrade.setBuyOrderId(taker.getOrderId()); // 设置买方的id
+
+        exchangeTrade.setBuyTurnover(taker.getTurnover()); // 设置买方的交易额
+        exchangeTrade.setSellTurnover(marker.getTurnover()); // 设置卖方的交易额
+
+        /**
+         * 处理盘口:
+         *  我们的委托单肯定是: 将挂单的数据做了一部分消耗
+         */
+        if (marker.getOrderDirection() == OrderDirection.BUY) {
+            // 减少挂单的数据量
+            orderBooks.getBuyTradePlate().remove(marker, turnoverAmount);
+        } else {
+            orderBooks.getSellTradePlate().remove(marker, turnoverAmount);
+        }
+
+        return exchangeTrade;
+    }
+
+    private BigDecimal calcTradeAmount(Order order) {
+        return order.getAmount().subtract(order.getTradedAmount());
     }
 
     private void completedOrders(List<Order> completedOrders) {
