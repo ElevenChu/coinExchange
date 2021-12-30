b91c285297abe48dc6c9a0a30ca7b4c6e3398285
elevenChu <964737042@qq.com>
2021-12-30 11:43:53 +0800

    12.30-VO的修改

--- exchange-engine/exchange-service/src/main/java/com/elevenchu/vo/DepthItemVo.java
+++ exchange-engine/exchange-service/src/main/java/com/elevenchu/vo/DepthItemVo.java
@@ -7,16 +7,37 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@ApiModel("明细")
+@ApiModel("深度盘口数据")
 public class DepthItemVo {
 
-    @ApiModelProperty(value = "价格")
-    private BigDecimal price=BigDecimal.ZERO;
+    /**
+     * 委托买单
+     */
+    @ApiModelProperty(value = "委托买单")
+    private List<com.elevenchu.domain.DepthItemVo> bids = Collections.emptyList() ;
 
-    @ApiModelProperty(value = "数量")
-    private BigDecimal volume=BigDecimal.ZERO;
+    /**
+     * 委托卖单
+     */
+    @ApiModelProperty(value = "委托卖单")
+    private List<com.elevenchu.domain.DepthItemVo> asks = Collections.emptyList() ;
+
+    /**
+     * 当前成交价(GCN)
+     */
+    @ApiModelProperty(value = "当前成交价GCN")
+    private BigDecimal price = BigDecimal.ZERO;
+
+    /**
+     * 当前成交价对应CNY价格
+     */
+    @ApiModelProperty(value = "当前成交价CNY")
+    private BigDecimal cnyPrice =  BigDecimal.ZERO;
+
 }
