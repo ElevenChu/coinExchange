821f2fc53d3d3b81a190678035db45faa31d511c
elevenChu <964737042@qq.com>
2021-12-31 16:23:38 +0800

    12.31-和前端适配

--- coin-chan/chan-service/src/main/java/com/elevenchu/handler/WebSocketMessageHandler.java
+++ coin-chan/chan-service/src/main/java/com/elevenchu/handler/WebSocketMessageHandler.java
@@ -1,13 +1,21 @@
 package com.elevenchu.handler;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.elevenchu.vo.ResponseEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.tio.core.ChannelContext;
 import org.tio.core.Tio;
 import org.tio.http.common.HttpRequest;
 import org.tio.http.common.HttpResponse;
 import org.tio.websocket.common.WsRequest;
 import org.tio.websocket.server.handler.IWsMsgHandler;
+
+import java.util.Objects;
 
 @Component
 @Slf4j
@@ -72,9 +80,45 @@ public class WebSocketMessageHandler  implements IWsMsgHandler {
      */
 
     @Override
-    public Object onText(WsRequest wsRequest, String s, ChannelContext channelContext) throws Exception {
+    public Object onText(WsRequest wsRequest, String text, ChannelContext channelContext) throws Exception {
+        if(Objects.equals("ping",text)){
+            return "pong";
+        }
+        log.info(text);
 
-        return null;
+        JSONObject payload = JSON.parseObject(text);
+
+        String sub = payload.getString("sub");//要订阅的组
+        String req = payload.getString("req"); // 当前的request(预留字段)
+        String cancel = payload.getString("cancel");// 要取消订阅的组
+        String id = payload.getString("id"); // 订阅的id(预留字段)
+        String authorization = payload.getString("authorization");
+
+        if(StringUtils.hasText(sub)){// 订阅的组有内容
+            Tio.bindGroup(channelContext,sub);
+        }
+        if(StringUtils.hasText(cancel)){
+            Tio.unbindGroup(cancel,channelContext);
+        }
+        if(StringUtils.hasText(authorization)&&authorization.startsWith("bearer ")){
+            String token = authorization.replaceAll("bearer ", "");
+            //2.查询我们的菜单数据
+            Jwt jwt = JwtHelper.decode(token);
+            String jwtJsonStr = jwt.getClaims();
+            JSONObject jwtJson = JSON.parseObject(jwtJsonStr);
+            String userId = jwtJson.getString("user_name");
+            // 有用户时绑定用户
+            Tio.unbindUser(channelContext.getTioConfig(),userId);
+        }
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setCanceled(cancel) ;
+        responseEntity.setSubbed(sub) ;
+        responseEntity.setId(id) ;
+        responseEntity.setStatus("OK") ;
+        responseEntity.setCh(sub) ;
+        responseEntity.setEvent(req) ;
+        return responseEntity.build() ;
+
 
     }
 }
--- coin-chan/chan-service/src/main/java/com/elevenchu/rocket/RocketMessageListener.java
+++ coin-chan/chan-service/src/main/java/com/elevenchu/rocket/RocketMessageListener.java
@@ -1,10 +1,14 @@
 package com.elevenchu.rocket;
 
 import com.alibaba.fastjson.JSON;
+import com.elevenchu.model.MessagePayload;
+import com.elevenchu.vo.ResponseEntity;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.tio.core.Tio;
 import org.tio.websocket.common.WsResponse;
 import org.tio.websocket.starter.TioWebSocketServerBootstrap;
@@ -17,10 +21,18 @@ public class RocketMessageListener {
 
 
     @StreamListener("tio_group")
-    public void handlerMessage(String message){
+    public void handlerMessage(MessagePayload message){
         log.info("接受到RocketMq的消息是=====>", JSON.toJSONString(message));
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setCh(message.getChannel());
+        responseEntity.put("result", message.getBody());
         //推送给前端用户
-        Tio.sendToGroup(tioWebSocketServerBootstrap.getServerTioConfig(),"test",WsResponse.fromText("发送消息","utf-8") );
+        if(StringUtils.hasText(message.getUserId())){
+            Tio.sendToUser(tioWebSocketServerBootstrap.getServerTioConfig(), message.getUserId(), responseEntity.build());
+            return;
+        }
+        String group = message.getChannel();
+        Tio.sendToGroup(tioWebSocketServerBootstrap.getServerTioConfig(), group, responseEntity.build());
     }
 
 }
