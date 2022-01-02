269c5545663de9c00dc9372f62182079af290508
elevenChu <964737042@qq.com>
2022-01-02 16:24:00 +0800

    2022.1.2-行情K线的生成

--- coin-task/src/main/java/com/elevenchu/event/TradeKLineEvent.java
+++ coin-task/src/main/java/com/elevenchu/event/TradeKLineEvent.java
@@ -1,13 +1,40 @@
 package com.elevenchu.event;
 
+import com.elevenchu.dto.CreateKLineDto;
+import com.elevenchu.dto.MarketDto;
+import com.elevenchu.feign.MarketServiceFeign;
+import com.elevenchu.service.TradeKlineService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * 行情数据的K线
+ */
 @Component
 @Slf4j
 public class TradeKLineEvent implements Event {
+
+
+    @Autowired
+    private MarketServiceFeign marketServiceFeign ;
+
     @Override
     public void handle() {
-
+        List<MarketDto> marketDtos = marketServiceFeign.tradeMarkets();
+        if (CollectionUtils.isEmpty(marketDtos)){
+            return;
+        }
+        for (MarketDto marketDto : marketDtos) {
+            CreateKLineDto createKLineDto = new CreateKLineDto();
+            createKLineDto.setVolume(BigDecimal.ZERO);
+            createKLineDto.setPrice(marketDto.getOpenPrice());
+            createKLineDto.setSymbol(marketDto.getSymbol().toLowerCase());
+            TradeKlineService.queue.offer(createKLineDto) ;
+        }
     }
 }
